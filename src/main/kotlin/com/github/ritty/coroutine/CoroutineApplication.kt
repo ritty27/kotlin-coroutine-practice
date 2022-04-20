dc38f19c2731b43dac9c96c7d072d8860c9d6621
ritty27 <jspark9812@gmail.com>
2022-04-20 23:54:08 +0900

    chore: init setting

---
+++ src/main/kotlin/com/github/ritty/coroutine/CoroutineApplication.kt
@@ -0,0 +1,11 @@
+package com.github.ritty.coroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutineApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoroutineApplication>(*args)
+}
